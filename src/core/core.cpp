8044fda6d9fb36c0b0eefc7ea4d7d86eaf07586b
peeweep <peeweep@0x0.ee>
2019-12-18 13:49:22 +0800

    Update command line options
    * set as target if no options
    * print help if no argv

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -12,37 +12,60 @@ void printFile(const std::filesystem::path &path) {
   }
 }
 
-int main(int argc, char const *argv[]) {
-  boost::program_options::options_description desc("Allowed options");
+void print_help_and_exit(
+    boost::program_options::options_description description) {
+  std::cout << "Super Symlinks Manager. [SuperSM] Version " << VERSION
+            << std::endl;
+  std::cout << description;
+  exit(1);
+}
 
+int main(int argc, char const *argv[]) {
   std::string default_target = "DIR";
+
+  // add options
+  boost::program_options::options_description desc("Allowed options");
   desc.add_options()("version,v", "version message")("help,h", "help message")(
       "target,t",
       boost::program_options::value<std::filesystem::path>()->default_value(
           default_target),
       "set target");
 
-  boost::program_options::variables_map vm;
+  // store map
+  boost::program_options::variables_map variablesMap;
   boost::program_options::store(
-      boost::program_options::parse_command_line(argc, argv, desc), vm);
-  boost::program_options::notify(vm);
+      boost::program_options::parse_command_line(argc, argv, desc),
+      variablesMap);
+  boost::program_options::notify(variablesMap);
 
-  if (vm.count("version")) {
+  // set as target if no options
+  boost::program_options::positional_options_description p;
+  p.add("target", -1);
+  boost::program_options::store(
+      boost::program_options::command_line_parser(argc, argv)
+          .options(desc)
+          .positional(p)
+          .run(),
+      variablesMap);
+
+  // print version and exit
+  if (variablesMap.count("version")) {
     std::cout << VERSION << std::endl;
-    return 1;
-  } else {
-    std::cout << "Super Symlinks Manager. [SuperSM] Version " << VERSION
-              << std::endl;
-    std::cout << desc << std::endl;
+    exit(1);
   }
 
-  if (vm["target"].as<std::filesystem::path>().string() != default_target) {
+  // print help and exit
+  if (variablesMap.count("help") or argc <= 1)
+    print_help_and_exit(desc);
+
+  // print target and exit
+  if (variablesMap["target"].as<std::filesystem::path>().string() !=
+      default_target) {
     std::cout << "Target was set to "
-              << vm["target"].as<std::filesystem::path>() << std::endl;
+              << variablesMap["target"].as<std::filesystem::path>()
+              << std::endl;
+    exit(1);
   }
-
-  //  else
-  //    std::cout << "Target was set to default" << std::endl;
 
   return 0;
 }
