e6b72dd54914c45244e2ff20fcf5d6669e8a1515
peeweep <peeweep@0x0.ee>
2019-12-18 19:14:19 +0800

    Update command_line_parser
     * allow_unregistered;
     * delete duplicated parser.

--- src/common.h
+++ src/common.h
@@ -10,24 +10,25 @@
 #include <boost/program_options.hpp>
 #include <filesystem>
 #include <iostream>
+
 class common {
  public:
-  void print_help_and_exit(
-      boost::program_options::options_description description) {
+  static void print_help_and_exit(
+      const boost::program_options::options_description& description) {
     std::cout << "Super Symlinks Manager. [SuperSM] Version " << VERSION;
     std::cout << std::endl;
     std::cout << description;
     exit(1);
   }
 
-  boost::program_options::variables_map process_options(int         argc,
-                                                        char const *argv[]) {
+  static boost::program_options::variables_map process_options(
+      int argc, char const* argv[]) {
     std::string default_target = "DIR";
 
     // add options
     boost::program_options::options_description desc("Allowed options");
-    desc.add_options()("version,v", "version message")("help,h",
-                                                       "help message")(
+    desc.add_options()("version,v", "version message")(
+        "SuperSM,s", "SuperSM directory")("help,h", "help message")(
         "target,t",
         boost::program_options::value<std::filesystem::path>()->default_value(
             default_target),
@@ -35,10 +36,6 @@ class common {
 
     // store map
     boost::program_options::variables_map variablesMap;
-    boost::program_options::store(
-        boost::program_options::parse_command_line(argc, argv, desc),
-        variablesMap);
-    boost::program_options::notify(variablesMap);
 
     // set as target if no options
     boost::program_options::positional_options_description p;
@@ -46,9 +43,11 @@ class common {
     boost::program_options::store(
         boost::program_options::command_line_parser(argc, argv)
             .options(desc)
+            .allow_unregistered()
             .positional(p)
             .run(),
         variablesMap);
+    boost::program_options::notify(variablesMap);
 
     // print version and exit
     if (variablesMap.count("version")) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+//
+// Created by peeweep on 2019-12-18.
+//
+
 #include "common.h"
+
 int main(int argc, char const *argv[]) {
-  common common;
-  common.process_options(argc, argv);
+  common::process_options(argc, argv);
   return 0;
 }
